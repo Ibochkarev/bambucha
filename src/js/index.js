ea74103041bef2bf8d38c03e429c1281b26b94f0
Bochkarev Ivan <ivanx86@gmail.com>
2022-11-30 12:21:03 +0600

    Добавлен GSAP, обновление изображений (иконок), замена текстов

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,31 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".s-info__lemon", {
+  scrollTrigger: {
+    trigger: ".s-info__lemon",
+    scrub: true,
+    pin: true,
+    start: "center center",
+    end: "bottom",
+    toggleClass: "active",
+    ease: "power2",
+  },
+});
+
+gsap.to(".s-info__sheat", {
+  scrollTrigger: {
+    trigger: ".s-info__sheat",
+    scrub: true,
+    pin: true,
+    start: "center center",
+    end: "bottom",
+    toggleClass: "active",
+    ease: "power2",
+  },
+});
+
 import "./import/sections";
 import "./import/components";
